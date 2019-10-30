13b0ef6f9fd8ddbceeba9ccadc11e454bc4cba85
Jesse <jesse.kuntz@varsitytutors.com>
2019-10-30 13:32:04 -0700

    Set up account screen

--- code/canvas.tsx
+++ code/canvas.tsx
@@ -1,3 +1,14 @@
 // WARNING: this file is auto generated, any changes will be lost
 import { createDesignComponent, CanvasStore } from "framer"
 const canvas = CanvasStore.shared(); // CANVAS_DATA;
+export const Accordian_Header_Collapsed_ = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string,Label?:string}>(canvas, "id_w9dBnnhyI", {Label:"string"}, 588,59);
+export const Navbar = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_r94aTcCfU", {}, 1440,108);
+export const Page_Title = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_Gb5BRW9_8", {}, 1440,93);
+export const Sidebar_Menu_Item = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string,Label?:string}>(canvas, "id_y3Lsqvzkd", {Label:"string"}, 245,19);
+
+export const colors = Object.freeze({
+    /** #153155 */
+    "Navy": "var(--token-06464679-c56f-4cff-bd28-60bb0c6851ee, rgb(21, 49, 85))",
+    /** #E6F2F7 */
+    "Light Blue": "var(--token-c4aaa2da-1b29-45fa-854e-b8d2a5887aef, rgb(230, 242, 247))",
+})
